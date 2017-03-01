485b5206f32b60c9da7981d6721b962f50334be7
Umut Ozel <umutozel@gmail.com>
2017-03-01 23:57:14 +0300

    Adding 4th solution

---
+++ 004 - Largest Palindrome Product/004 - Largest_Palindrome_Product.fs
@@ -0,0 +1,11 @@
+open System.Linq
+
+let isPalindromic n =
+    let s = n.ToString().ToCharArray()
+    s.SequenceEqual (Array.rev s)
+ 
+let products =  [for x in 100..999 do
+                 for y in 100..999 do
+                    yield x*y]
+
+printfn "%i" (products |> Seq.filter isPalindromic |> Seq.max)
